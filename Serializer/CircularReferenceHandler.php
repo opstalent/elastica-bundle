8984ace1f66679108d290acfc47d90ad9ce808d0
Patryk Grudniewski <patgrudniewski@gmail.com>
2017-05-16 17:08:47 +0200

    Circular Reference Handler

---
+++ Serializer/CircularReferenceHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Opstalent\ElasticaBundle\Serializer;
+
+/**
+ * @author Patryk Grudniewski <patgrudniewski@gmail.com>
+ * @package Opstalent\ElasticaBundle
+ */
+class CircularReferenceHandler
+{
+    /**
+     * @param object $object
+     * @return mixed
+     */
+    public static function handle($object)
+    {
+        return null;
+    }
+}
